33075dc5d096a88f39832c5565f65ca7dcf2ea98
Yael Lozano <lozanoy6@gmail.com>
2021-05-18 00:57:22 -0500

    Added a new function

--- assets/script.js
+++ assets/script.js
@@ -30,6 +30,22 @@ closewindow.forEach(window => {
     })
 })
 
+function refreshCSS() {
+    var sheets = [].slice.call(document.getElementsByTagName("link"));
+    var head = document.getElementsByTagName("head")[0];
+    for (var i = 0; i < sheets.length; ++i) {
+        var elem = sheets[i];
+        var parent = elem.parentElement || head;
+        parent.removeChild(elem);
+        var rel = elem.rel;
+        if (elem.href && typeof rel != "string" || rel.length == 0 || rel.toLowerCase() == "stylesheet") {
+            var url = elem.href.replace(/(&|\?)_cacheOverride=\d+/, '');
+            elem.href = url + (url.indexOf('?') >= 0 ? '&' : '?') + '_cacheOverride=' + (new Date().valueOf());
+        }
+        parent.appendChild(elem);
+    }
+}
+
 function navSlide() {
     burger.addEventListener("click", () => {
         navBar.classList.toggle("navigation-active");
@@ -57,3 +73,4 @@ if (vw <= 768) {
     });
 }
 navSlide();
+refreshCSS();
